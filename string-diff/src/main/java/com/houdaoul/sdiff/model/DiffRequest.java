2ace84230a1b7d1ca38aaecffd2eca7f1719957d
Houda Oul <houda.oulachguer@usmba.ac.ma>
2020-09-02 10:42:15 +0100

    DiffRequest updated, removed getters and setters

--- string-diff/src/main/java/com/houdaoul/sdiff/model/DiffRequest.java
+++ string-diff/src/main/java/com/houdaoul/sdiff/model/DiffRequest.java
@@ -22,20 +22,4 @@ public class DiffRequest {
 
 	@NotNull
 	private String second;
-
-	public String getFirst() {
-		return first;
-	}
-
-	public void setFirst(String first) {
-		this.first = first;
-	}
-
-	public String getSecond() {
-		return second;
-	}
-
-	public void setSecond(String second) {
-		this.second = second;
-	}
 }
